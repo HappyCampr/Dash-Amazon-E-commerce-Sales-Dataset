504145f3c16782a19d788288581f56b697e417b6
Jenna <jennasemail4321@gmail.com>
2026-02-25 23:29:26 -0800

    Correct deprecated app code

--- app.py
+++ app.py
@@ -707,4 +707,4 @@ def update_dashboard(category, state, sub_category, start_date, end_date, curren
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
