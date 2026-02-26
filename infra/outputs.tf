0ae0c819623dd84b0d8c1e67426a958ebf6a60dc
Jenna <jennasemail4321@gmail.com>
2026-02-25 23:08:19 -0800

    Removing artifacts from output too

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,6 +2,6 @@ output "service_url" {
   value = google_cloud_run_v2_service.app.uri
 }
 
-output "artifact_repo" {
-  value = google_artifact_registry_repository.dash.name
-}
+#output "artifact_repo" {
+#  value = google_artifact_registry_repository.dash.name
+#}
