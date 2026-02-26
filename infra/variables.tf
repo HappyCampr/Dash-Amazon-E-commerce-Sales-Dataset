e99dc81f219fec77a18e2ab4744a3e5d8bfe6152
Jenna <jennasemail4321@gmail.com>
2026-02-25 20:30:19 -0800

    Correcting variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,16 @@
-variable "project_id" { type = string }
-variable "region"     { type = string  default = "us-central1" }
+variable "project_id" {
+  type = string
+}
 
-# GitHub Actions will produce this:
-# us-central1-docker.pkg.dev/PROJECT_ID/dash/dashapp:<gitsha>
-variable "image" { type = string }
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "image" {
+  type        = string
+  description = "Artifact Registry image URI for the Cloud Run service"
+}
 
 variable "service_name" {
   type    = string
